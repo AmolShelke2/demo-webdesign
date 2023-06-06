35d70c844e78037ca6733f493377fb4f882da858
Amol <shelkeamol897@gmail.com>
2023-06-06 16:52:56 +0530

    [Fixed]: Build error

--- src/components/InformationCards.tsx
+++ src/components/InformationCards.tsx
@@ -6,7 +6,9 @@ const InformationCards = () => {
   return (
     <div className="grid place-items-center sm:place-items-start px-2 sm:grid-cols-2 lg:grid-cols-3 gap-2 lg:px-24">
       {cardsData.map((card) => (
-        <div className="h-[250px] w-[300px] xl:w-[380px] bg-slate-100 shadow-sm shadow-[#C3A9FF] mb-2 rounded-sm md:mr-2 gap-4">
+        <div
+          className="h-[250px] w-[300px] xl:w-[380px] bg-slate-100 shadow-sm shadow-[#C3A9FF] mb-2 rounded-sm md:mr-2 gap-4"
+          key={Math.random()}>
           <h1 className="bg-[#3F3F3F] text-white text-xl p-2 mb-2">
             {card.title}
           </h1>
